470020ea851ce5c40bb5f4aa0f21a840e957f4fd
periyaraja-s <periyaraja1706@gmail.com>
2026-09-03 20:05:24 +0530

    Authenticate dashboard smoke test after protected home route

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,6 +13,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        $this->actingAs(User::factory()->create(['role' => 'viewer']));
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
