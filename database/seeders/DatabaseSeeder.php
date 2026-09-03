133564ddceb9c904a08b144767bfd2aa566e32dc
periyaraja-s <periyaraja1706@gmail.com>
2026-09-03 19:10:20 +0530

    Complete authentication and role protection: database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             BuyerSeeder::class,
             StyleSeeder::class,
             SewingLineSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
