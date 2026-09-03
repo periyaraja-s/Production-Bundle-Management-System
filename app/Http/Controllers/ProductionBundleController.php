36d8f6cdcad4200ae9be4bb242e837f36b9702d6
periyaraja-s <periyaraja1706@gmail.com>
2026-09-03 19:44:32 +0530

    Keep bundle controller compatible with role-based UI

--- app/Http/Controllers/ProductionBundleController.php
+++ app/Http/Controllers/ProductionBundleController.php
@@ -10,6 +10,7 @@ use App\Models\Style;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class ProductionBundleController extends Controller
