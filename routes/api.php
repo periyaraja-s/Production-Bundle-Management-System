c5217f1b93034210a544617860fb3ec669471591
periyaraja-s <periyaraja1706@gmail.com>
2026-09-03 19:09:00 +0530

    Protect application with authentication: routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,18 @@
 
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\ProductionBundleController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/bundles', [ProductionBundleController::class, 'index']);
-Route::post('/bundles', [ProductionBundleController::class, 'store']);
-Route::put('/bundles/{bundle}', [ProductionBundleController::class, 'update']);
-Route::delete('/bundles/{bundle}', [ProductionBundleController::class, 'destroy']);
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', fn (Request $request) => $request->user());
+
+    Route::get('/bundles', [ProductionBundleController::class, 'index']);
+    Route::post('/bundles', [ProductionBundleController::class, 'store']);
+    Route::put('/bundles/{bundle}', [ProductionBundleController::class, 'update']);
+    Route::delete('/bundles/{bundle}', [ProductionBundleController::class, 'destroy']);
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+});
